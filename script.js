eef2d49ffde16fca2588242602bcf291b6b85405
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-06-18 18:58:04 +0100

    updated with comment

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Task 01: Iterating with Async/Await
+// Write an async function iterateWithAsyncAwait that takes an array of values and logs each value with a delay of 1 second between logs.
+
 let delayTime = 1000 // Delay time of 1 second
 let userInput = prompt("Enter a list of items separated by commas:");
 let arr = userInput.split(",");
